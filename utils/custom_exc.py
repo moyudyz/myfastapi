8f4ec3a42e1e4f45c217092b3ac22cdf20b16d64
moyudyz <3030590755@qq.com>
2020-09-28 01:15:53 +0800

    更改日志格式

--- utils/custom_exc.py
+++ utils/custom_exc.py
@@ -2,7 +2,7 @@
 Author: moyu
 Date: 2020-09-27 15:23:22
 LastEditors: moyu
-LastEditTime: 2020-09-28 01:03:38
+LastEditTime: 2020-09-28 01:15:36
 Description: 自定义异常处理
 FilePath: /myfastapi/utils/custom_exc.py
 '''
@@ -41,40 +41,40 @@ def register_exc(app: FastAPI) -> None:
     @app.exception_handler(UnicornException)
     async def unicorn_exception_handler(request: Request, exc: UnicornException):
         logger.debug(
-            f"认证异常\n{request.method} URL:{request.url}\n{exc.err_desc}")
+            f"认证异常\n{request.method} | URL:{request.url} | {exc.err_desc}")
         return response_code.resp_400(exc.err_desc)
 
     # UserTokenError异常处理
     @app.exception_handler(UserTokenException)
     async def token_exception_handler(request: Request, exc: UserTokenException):
         logger.debug(
-            f"认证异常\n{request.method} URL:{request.url}\n{exc.err_desc}")
+            f"认证异常\n{request.method} | URL:{request.url} | {exc.err_desc}")
         return response_code.resp_403(exc.err_desc)
 
     # NotFoundError异常处理
     @app.exception_handler(NotFoundException)
     async def notfount_exception_handler(request: Request, exc: NotFoundException):
         logger.debug(
-            f"无效请求\n{request.method} URL:{request.url}\n{exc.err_desc}")
+            f"无效请求\n{request.method} | URL:{request.url} | {exc.err_desc}")
         return response_code.resp_404(exc.err_desc)
 
     # 覆盖HTTPException异常处理
     @app.exception_handler(HTTPException)
     async def http_exception_handler(request: Request, exc: HTTPException):
         logger.debug(
-            f"HTTP异常\n{request.method} URL:{request.url}\n{exc.detail}")
+            f"HTTP异常\n{request.method} | URL:{request.url} | {exc.detail}")
         return response_code.resp_400(exc.detail)
 
     # 覆盖RequestValidationError异常处理
     @app.exception_handler(RequestValidationError)
     async def validation_exception_handler(request: Request, exc: RequestValidationError):
         logger.debug(
-            f"参数异常\n{request.method} URL:{request.url}\n{exc}")
+            f"参数异常\n{request.method} | URL:{request.url}\n{exc}")
         return response_code.resp_400(str(exc))
 
     # 覆盖全部异常处理
     @app.exception_handler(Exception)
     async def all_exception_handler(request: Request, exc: HTTPException):
         logger.error(
-            f"全局异常\n{request.method} URL:{request.url}\nHeaders:{request.headers}\n{traceback.format_exc()}")
+            f"全局异常\n{request.method} | URL:{request.url}\nHeaders:{request.headers}\n{traceback.format_exc()}")
         return response_code.resp_500()
